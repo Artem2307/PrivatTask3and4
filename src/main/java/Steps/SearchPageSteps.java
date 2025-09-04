ec63cc3c798f6ef85a12e8ac0604c32e128080ca
Artem2307 <Pravdyk1@gmail.com>
2025-09-04 05:42:45 +0300

    fixed

--- src/main/java/Steps/SearchPageSteps.java
+++ src/main/java/Steps/SearchPageSteps.java
@@ -6,7 +6,7 @@ import pages.SearchPage;
 public class SearchPageSteps extends SearchPage {
 
     public SearchPageSteps clickVideoByIndexStep(int index) {
-        Allure.step("Клик по видео з списку під номером: " + index, () -> {
+        Allure.step("Клік по відео з списку під номером: " + index, () -> {
             clickVideoByIndexInternal(index);
         });
         return this;
